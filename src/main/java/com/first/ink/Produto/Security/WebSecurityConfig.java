eefd153cdabf3267f58e311922490b2bdba0639a
unknown <adelsonsantospinheiro@gmail.com>
2020-08-21 20:39:59 -0300

    atualização rápida da tela de manutenção

--- src/main/java/com/first/ink/Produto/Security/WebSecurityConfig.java
+++ src/main/java/com/first/ink/Produto/Security/WebSecurityConfig.java
@@ -46,4 +46,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception{
 		web.ignoring().antMatchers("/materialize/**", "/style/**" , "/images/**" );
 	}
+	
+	
 }
